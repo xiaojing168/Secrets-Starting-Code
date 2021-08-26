9b7f241e1d93998309a01df9107d7748f14becc2
Justin Greene <justingreene@xjds-imac.attlocal.net>
2021-08-26 10:19:37 -0400

    level 2 - Database Encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -13,12 +13,13 @@ app.use(bodyParser.urlencoded({
   extended: true}));
 mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser: true});
 
-
-
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-});
+})
+const secret = "howsareyouaskdjfialksdjflkaj@alkjdfia"
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+const User = mongoose.model('User', userSchema);
 
 
 app.get('/',function(req,res){
