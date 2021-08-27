3bc66c0524a3aacab635d08c4195bb343caf1833
Justin Greene <justingreene@xjds-imac.attlocal.net>
2021-08-26 22:10:25 -0400

    Level 3 -Hashing Passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 const User = mongoose.model('User', userSchema);
 
 
@@ -39,7 +39,7 @@ app.post('/register',function(req,res){
 
   const newUser = new User({
   email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -52,7 +52,7 @@ app.post('/register',function(req,res){
 
 app.post('/login',function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 User.findOne({email: username},function(err,foundUser){
   if(err){
     console.log(err);
